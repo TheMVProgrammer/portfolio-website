0fa00f8df806f3f39158903849a66e3dbba36bd2
TheMVProgrammer <franklin0415@hotmail.com>
2022-12-31 15:15:55 -0400

    Finishing Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Hi everyone I'm Franklin Martinez, currently on my journey to become a Frontend Software Developer. I'm interested in a lot of topics like design, UX, UI even in fields like mobile development and game development. I am currently learning JavaScript and React.js.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
